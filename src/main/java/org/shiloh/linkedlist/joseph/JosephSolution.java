9a88dca6f3662c0c668630abd14b0bf452a80f91
shiloh <lixiaolei595@gmail.com>
2022-07-12 23:40:23 +0800

    feat: 新增约瑟夫问题解决方案与测试代码

--- src/main/java/org/shiloh/linkedlist/joseph/JosephSolution.java
+++ src/main/java/org/shiloh/linkedlist/joseph/JosephSolution.java
@@ -11,9 +11,13 @@ public class JosephSolution {
         // 单向环形链表构建测试
         final OneWayCircularLinkedList oneWayCircularLinkedList = new OneWayCircularLinkedList();
         // 添加指定数量的节点
-        oneWayCircularLinkedList.add(25);
+        oneWayCircularLinkedList.add(125);
         // 输出链表内容
-        oneWayCircularLinkedList.print();
+        // oneWayCircularLinkedList.print();
+
+        // 约瑟夫问题：测试小男孩出圈
+        // 预期出圈顺序：2 --> 4 --> 1 --> 5 --> 3
+        oneWayCircularLinkedList.countBoy(10, 20, 25);
     }
 }
 
@@ -100,6 +104,56 @@ class OneWayCircularLinkedList {
      */
     public boolean isEmpty() {
         return this.first == null;
+    }
+
+    /**
+     * 约瑟夫问题：计算小男孩出圈的顺序
+     *
+     * @param startNo  表示从第几个小男孩开始计数
+     * @param countNum 表示数几下
+     * @param nums     表示最初有多少个小男孩在圈内
+     * @author shiloh
+     * @date 2022/7/12 23:18
+     */
+    public void countBoy(int startNo, int countNum, int nums) {
+        if (this.isEmpty() || startNo < 1 || startNo > nums) {
+            System.out.println("参数输入错误，请重新输入");
+            return;
+        }
+
+        // 创建辅助变量，用于帮助小男孩出圈
+        // 通过遍历环形链表，让辅助变量事先指向环形链表的最后一个节点
+        Boy helper = first;
+        // 当辅助变量的 next 引用指向头节点时，说明辅助变量已经指向了环形链表中的最后一个节点
+        while (helper.getNext() != this.first) {
+            // 移动辅助变量，继续往下走
+            helper = helper.getNext();
+        }
+
+        // 在开始计数前，将 first 和辅助变量移动 k - 1 次，使其指向计数起始节点
+        for (int j = 0; j < startNo - 1; j++) {
+            this.first = this.first.getNext();
+            helper = helper.getNext();
+        }
+
+        // 开始计数：将 first 和辅助变量移动 m - 1 次，然后将 first 指向的节点从环形链表中删除，即出圈操作
+        // 出圈为一个循环操作，直到圈中只有一个节点才停止
+        // 当 first == 辅助变量时，说明圈中只有一个节点
+        while (helper != this.first) {
+            // 将 first 和辅助变量同时移动 countNum - 1 次
+            for (int i = 0; i < countNum - 1; i++) {
+                this.first = this.first.getNext();
+                helper = helper.getNext();
+            }
+
+            // 走完后，first 指向的节点就是要删除的节点
+            System.out.printf("编号为：%d 的小男孩出圈\n", this.first.getNo());
+            this.first = this.first.getNext();
+            // 将辅助变量的 next 引用指向 first，形成一个环状，继续循环
+            helper.setNext(this.first);
+        }
+
+        System.out.printf("最后留在圈中的小男孩的编号为：%d\n", this.first.getNo());
     }
 }
 
