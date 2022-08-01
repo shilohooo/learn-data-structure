1abce4deb1d8c40eea6fcc132455ad75ea6bf91f
shiloh <lixiaolei595@gmail.com>
2022-08-01 22:47:14 +0800

    feat: 新增方法：将中缀表达式转换为后缀表达式，以及相关测试代码

--- src/main/java/org/shiloh/stack/PolandNotation.java
+++ src/main/java/org/shiloh/stack/PolandNotation.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+import static org.shiloh.stack.constant.OperatorConstants.getOperatorPriority;
+
 /**
  * 基于后缀表达式实现一个逆波兰计算器
  * <p>
@@ -13,22 +15,28 @@ import java.util.Stack;
  * @date 2022/7/21 22:05
  */
 public class PolandNotation {
+
+    /**
+     * 左括号
+     */
+    public static final String LEFT_BRACE = "(";
+
+    /**
+     * 右括号
+     */
+    public static final String RIGHT_BRACE = ")";
+
     public static void main(String[] args) {
-//        测试中缀表达式字符串转中缀表达式字符串列表
+        // 测试中缀表达式字符串转中缀表达式字符串列表
         final String infixExpr = "1+((2+3)*4)-5";
         final List<String> infixExprList = toInfixExprList(infixExpr);
-        System.out.println(infixExprList);
-
-//        // 定义一个逆波兰表达式（后缀表达式）
-//        // 测试 - (3 + 4) * 5 - 6 => 3 4 + 5 * 6 - => 计算结果应为：29
-//        // 测试 - (30 + 4) * 5 - 6 => 30 4 + 5 * 6 - => 计算结果应为：164
-//        // 测试 - 4 * 5 - 8 + 60 + 8 / 2 => => 计算结果应为：76
-//        final String suffixExpr = "4 5 * 8 - 60 + 8 2 / +";
-//        // 将逆波兰表达式字符串以空格分隔，转换为 List
-//        final List<String> reversePolandExprList = getReversePolandExprList(suffixExpr);
-//        // 计算结果
-//        final int result = calcReversePolandExpr(reversePolandExprList);
-//        System.out.printf("逆波兰表达式：%s 的计算结果为：%d", suffixExpr, result);
+        System.out.println("中缀表达式字符串列表：" + infixExprList);
+        // 测试将中缀表达式字符串列表转换为后缀表达式字符串列表
+        final List<String> suffixExprList = toSuffixExprList(infixExprList);
+        System.out.println("后缀表达式字符串列表：" + suffixExprList);
+        // 计算结果
+        final int result = calcReversePolandExpr(suffixExprList);
+        System.out.printf("逆波兰表达式：%s 的计算结果为：%d", suffixExprList, result);
     }
 
     /**
@@ -68,6 +76,64 @@ public class PolandNotation {
         } while (index < length);
 
         return infixExprList;
+    }
+
+    /**
+     * 将中缀表达式字符串列表转换为后缀表达式字符串列表
+     *
+     * @param infixExprList 中缀表达式字符串列表
+     * @return 后缀表达式字符串列表
+     * @author shiloh
+     * @date 2022/8/1 21:56
+     */
+    public static List<String> toSuffixExprList(List<String> infixExprList) {
+        // 定义两个栈
+        // 运算符栈
+        final Stack<String> operatorStack = new Stack<>();
+        // 中间结果栈
+        // final Stack<String> midResultStack = new Stack<>();
+        // 因为中间结果栈在整个转换过程中没有涉及到出栈（pop）操作，且后面需要逆序输出
+        // 比较麻烦，这里直接使用字符串列表代替
+        final List<String> suffixExprList = new ArrayList<>();
+
+        // 遍历中间表达式字符串列表
+        for (final String item : infixExprList) {
+            if (item.matches("\\d+")) {
+                // 如果是一个数字，则直接加入到列表中
+                suffixExprList.add(item);
+            } else if (LEFT_BRACE.equals(item)) {
+                // 如果是一个左括号，则直接入运算符栈
+                operatorStack.push(item);
+            } else if (RIGHT_BRACE.equals(item)) {
+                // 如果是右括号，则依次弹出栈顶运算符，并添加到列表中，直到遇到左括号为止
+                // 此时将这一对括号丢弃
+                while (!LEFT_BRACE.equals(operatorStack.peek())) {
+                    suffixExprList.add(operatorStack.pop());
+                }
+                // 丢弃括号
+                operatorStack.pop();
+            } else {
+                // 如果当前遍历得到的运算符的优先级小于等于栈顶运算符
+                // 需要将栈顶运算符添加到列表中，然后再与新的栈顶运算符比较优先级
+
+                // 获取运算符的优先级
+                final int currentPriority = getOperatorPriority(item.charAt(0));
+                while (!operatorStack.isEmpty()
+                        && getOperatorPriority(operatorStack.peek().charAt(0)) >= currentPriority) {
+                    suffixExprList.add(operatorStack.pop());
+                }
+                // 将当前遍历得到的运算符入栈
+                operatorStack.push(item);
+            }
+        }
+
+        // 最后将栈中剩余的内容添加到列表中
+        while (!operatorStack.isEmpty()) {
+            suffixExprList.add(operatorStack.pop());
+        }
+
+        // 这里用的是列表，可以直接返回结果，无需逆序
+        return suffixExprList;
     }
 
     /**
@@ -134,3 +200,4 @@ public class PolandNotation {
         return Integer.parseInt(stack.pop());
     }
 }
+
